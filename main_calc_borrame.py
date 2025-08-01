ca8b15b766b1befdf3a9df6d3bfcf8c9c1d61d18
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-08-01 20:13:05 -0300

    Explicación sobre el uso de __init__.py en módulos.

---
+++ main_calc_borrame.py
@@ -0,0 +1,13 @@
+#from calculator.operations.basics import suma, resta
+
+def alguna_funcion_re_dificil_que_tengo_que_hacer_despues():
+    """Necesito hacer tal y cual cosa, no se bien como hay que tatata..."""
+    pass
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
