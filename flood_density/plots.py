11ad5bc1cbd9a557f365e4664ff2eed85561863a
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-08-03 19:56:43 -0300

    refactor: prints are deleted in convert_kml_to_geojson and export_to_geojson

--- flood_density/plots.py
+++ flood_density/plots.py
@@ -8,15 +8,12 @@ def convert_kml_to_geojson(kml_file: str, output_file: str) -> None:
     gdf = gpd.read_file(kml_file, driver="KML")
     gdf.to_file(output_file, driver='GeoJSON')
     gdf.plot()
-
-    print(f"Converted {kml_file} to {output_file}") 
     
     return output_file
 
 
 
-def export_to_geojson(gdf: gpd.GeoDataFrame, output_path) -> str:
+def export_to_geojson(gdf: gpd.GeoDataFrame, output_path: str) -> str:
     gdf.to_file(output_path, driver='GeoJSON')
-    print(f"Archivo exportado: {output_path}")
 
     return output_path
