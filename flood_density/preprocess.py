8163c298f290c43f138d988263c02782ec324eb6
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-08-04 09:28:36 -0300

    feat : extract_city_bounds_from_dataframe_to_geodataframe and extract_city_bounds are moved from jupyter notebook.

---
+++ flood_density/preprocess.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+def extract_city_bounds_from_dataframe_to_geodataframe(df: pd.DataFrame, lat_col: str, lon_col: str) -> gpd.GeoDataFrame:
+
+    geometry = [Point(xy) for xy in zip(df[lon_col], df[lat_col])]
+    gdf = gpd.GeoDataFrame(df.copy(), geometry=geometry, crs="EPSG:4326")
+
+    return gdf
+
+
+def extract_city_bounds(gdf: gpd.GeoDataFrame) -> Dict[str, float]:    
+# Extraer límites
+    minx, miny, maxx, maxy = gdf.total_bounds
+
+    return {
+        "west_longitude": minx,
+        "south_latitude": miny,
+        "east_longitude": maxx,
+        "north_latitude": maxy
+    }
