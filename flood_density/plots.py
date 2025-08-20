da67c7e3505200864de123500d05bc3757f26aa6
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-08-20 10:42:59 -0300

    refactor: delete function convert_kml_to_geosjon .

--- flood_density/plots.py
+++ flood_density/plots.py
@@ -7,28 +7,21 @@ from typing import List
 
 CRS_4326 = 4326
 
+#def convert_kml_to_gdf(kml_file: str) -> gpd.GeoDataFrame:
+    #gdf = gpd.read_file(kml_file, driver="KML")
+    #return gdf
 
-#def convert_kml_to_geojson(kml_file: str, output_file: str) -> str:
-#    gdf = gpd.read_file(kml_file, driver="KML")
-#    gdf.to_file(output_file, driver='GeoJSON')
-#    gdf.plot()
-#    
-#    return output_file
+#def export_to_geojson(gdf: gpd.GeoDataFrame, output_path: str) -> str:
+    #geojson = gdf.to_file(output_path, driver='GeoJSON')
 
-def convert_kml_to_geojson(kml_file: str) -> str:
-    gdf = gpd.read_file(kml_file, driver="KML")
-    gdf.to_file(output_file, driver='GeoJSON')
-    gdf.plot()
+    #return geojson
+
+#def plot_gdf(gdf: gpd.GeoDataFrame, str : "La Plata Geospatial Data") -> None:
     
-    return output_file
+    #return gdf.plot()
 
 
-def export_to_geojson(gdf: gpd.GeoDataFrame, output_path: str) -> str:
-    gdf.to_file(output_path, driver='GeoJSON')
-
-    return output_path
-
-def city_bounds_and_density_plot(gdf: gpd.GeoDataFrame, centroides: np.ndarray, values_density: np.ndarray, bound: List[float]) -> fig:
+def city_bounds_and_density_plot(gdf: gpd.GeoDataFrame, centroides: np.ndarray, values_density: np.ndarray, bound: List[float]) -> plt.Figure:
     
     fig, ax = plt.subplots(figsize=(12, 10))
 
