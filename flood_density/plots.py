e3ea2557aec8c240f37ee2ad78ba2396f30f1a04
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-08-27 19:22:41 -0300

    feat: add function plot_kriging_results_with_basemap().

--- flood_density/plots.py
+++ flood_density/plots.py
@@ -36,3 +36,65 @@ def city_bounds_and_density_plot(gdf: gpd.GeoDataFrame, centroides: np.ndarray, 
 
     return plt.show()
 
+def plot_kriging_results_with_basemap(gdf: gpd.GeoDataFrame, 
+                                      coords: np.ndarray, 
+                                      values: np.ndarray,
+                                      bounds: Tuple[float, float, float, float], 
+                                      grid_x: np.ndarray, 
+                                      grid_y: np.ndarray, 
+                                      model: OrdinaryKriging, 
+                                      kriging_result: Tuple[object, np.ndarray]) -> plt.Figure:
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    # Obtener la superficie interpolada desde la función predict_fn
+    grid_z, ss = kriging_result
+    
+    # Plotear superficie Kriging interpolada
+    contour = ax.contourf(grid_x, grid_y, grid_z, levels=30, cmap='viridis', alpha=0.5)
+    
+
+    # Plotear los polígonos originales
+    casco_urbano_utm.plot(column='Z', 
+               cmap='viridis',
+               alpha=0.5,
+               edgecolor='black',
+               linewidth=1.0,
+               ax=ax)
+
+    # Plotear los polígonos originales con bordes
+    casco_urbano_utm.plot(column='Z', 
+               cmap='viridis',
+               alpha=0.5,
+               edgecolor='black',
+               linewidth=1.0,
+               ax=ax)
+
+    # Plotear los puntos centroides
+    scatter = ax.scatter(coords[:, 0], coords[:, 1],
+                    c=gdf['Z'],
+                    cmap='viridis',
+                    s=30,
+                    edgecolors='black',
+                    linewidths=1,
+                    zorder=5)
+
+    # Ajustar límites del gráfico según los límites de La Plata
+    ax.set_xlim(bounds[0], bounds[2])
+    ax.set_ylim(bounds[1], bounds[3])
+
+    #Mapa base más sutil
+    ctx.add_basemap(ax, crs=casco_urbano_utm.crs.to_string(),
+                   source=ctx.providers.CartoDB.Positron,
+                   alpha = 0.9)
+
+    # Agregar colorbar para el scatter
+    plt.colorbar(scatter, ax=ax, label='Densidad poblacional',shrink=0.7, aspect=25)
+
+    # Agregar título y etiquetas
+    ax.set_title('Interpolación Kriging - Densidad Poblacional La Plata')
+    ax.set_xlabel('X (UTM)')
+    ax.set_ylabel('Y (UTM)')
+
+    plt.tight_layout()
+    plt.show()
