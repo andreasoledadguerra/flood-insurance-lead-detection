dedaf264d12b05bfb272ee4dfbeb1cd1c000ef8a
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-09-03 20:23:12 -0300

    refactor: rename "extract_coords()" to "extract_coords_from_geometry()"   and "extract_centroids()" to "extract_centroids_from_gdf()"

--- flood_density/preprocess.py
+++ flood_density/preprocess.py
@@ -116,7 +116,7 @@ def clip_density_to_urban_area(gdf_1: gpd.GeoDataFrame, gdf_2: gpd.GeoDataFrame)
     
     return points_in_casco
 
-def extract_centroids(gdf: gpd.GeoDataFrame, column_name: str) -> np.ndarray:
+def extract_centroids_from_gdf(gdf: gpd.GeoDataFrame, column_name: str) -> np.ndarray:
 
     # Extraer valores de una columna específica
     values = gdf[column_name].values
@@ -125,7 +125,7 @@ def extract_centroids(gdf: gpd.GeoDataFrame, column_name: str) -> np.ndarray:
     return values
 
 
-def extract_coords(gdf: gpd.GeoDataFrame) -> np.ndarray:
+def extract_coords_from_geometry(gdf: gpd.GeoDataFrame) -> np.ndarray:
 
     # Extraer centroides y convertir a un array de coordenadas
     centroids = np.array([[point.x, point.y] for point in gdf.geometry.centroid])
