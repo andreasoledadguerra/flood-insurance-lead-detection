35de771ce2f05ca3cc81e20812815438ed1f22d6
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-11-12 16:36:08 -0300

    feat: add title and set  style

--- main.py
+++ main.py
@@ -3,5 +3,20 @@ import streamlit as st
 from fastapi import FastAPI
 
 
-#st.title("Hola, personita curiosa!")
-#st.write("Le damos la bienvenida a la Aplicación de Detección de Inundaciones!")
+grad = """
+<style>
+.gradient-text {
+  font-size: 48px;
+  font-weight: 800;
+  background: linear-gradient(90deg, #f403d1, #04c2c9);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+</style>
+"""
+
+st.markdown(grad, unsafe_allow_html=True)
+st.markdown('<div class="gradient-text">Hola, personita curiosa!</div>', unsafe_allow_html=True)
+
+st.write("Le damos la bienvenida a la Aplicación de Detección de Inundaciones!")
