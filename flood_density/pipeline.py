c56295cf3e985c6664ebfe1e1db025727e11d261
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-15 21:47:37 -0300

    chore: import methods from flood_density/preprocess

--- flood_density/pipeline.py
+++ flood_density/pipeline.py
@@ -3,3 +3,12 @@ import numpy as np
 from geopandas import GeoDataFrame
 from sklearn.gaussian_process import GaussianProcessRegressor
 
+from flood_density.preprocess import (
+    clip_density_to_urban_area,
+    extract_coords_from_geometry,
+    extract_centroids_from_gdf,
+    interpolate_grid,
+    fit_gpr_model,
+    convert_to_2d_grid,
+)
+
