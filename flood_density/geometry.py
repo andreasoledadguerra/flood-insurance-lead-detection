f60274b672931782dd6d9e32856199c263e2b242
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-16 13:19:31 -0300

    chore: import Polygon from shapely.geometry

---
+++ flood_density/geometry.py
@@ -0,0 +1,14 @@
+
+from shapely.geometry import Polygon
+
+
+
+##Function that convert dictionary of coordinates in polygon
+#    def coordinates_to_box(coord : dict)-> Polygon:
+#    
+#        return box(
+#            coord['x_min'],
+#            coord['y_min'],
+#            coord['x_max'],
+#            coord['y_max'],
+#        )
