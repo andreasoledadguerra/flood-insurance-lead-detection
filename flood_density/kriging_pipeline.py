38cea354a25b07ace96aeb7db3d9c04ef8372e74
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-17 08:55:29 -0300

    refactor: move create_kring_kernel, create_gpr_model, fit_gpr_model, interpolate_grid, predict_grid and convert_to_2d_grid from PreProcessData to KrigingModel

--- flood_density/kriging_pipeline.py
+++ flood_density/kriging_pipeline.py
@@ -46,3 +46,61 @@ class KrigingModel:
         kernel = constant_kernel * rbf_kernel + noise_kernel
 
         return kernel
+    
+     # Generar el modelo GaussianProcessRegressor
+    def create_gpr_model(kernel: Kernel) -> GaussianProcessRegressor:
+        gpr = GaussianProcessRegressor(kernel=kernel, alpha=1e-6, n_restarts_optimizer=10)
+        return gpr
+
+    # Ajustar el modelo GPR a los datos
+    def fit_gpr_model(
+        gpr: GaussianProcessRegressor, coords: np.ndarray, values: np.ndarray) -> GaussianProcessRegressor:
+
+        gpr.fit(coords, values)
+        return gpr
+
+
+    def interpolate_grid(
+        bounds: Tuple[float, float, float, float],
+        step: int = 100
+    ) -> Tuple[np.ndarray, np.ndarray]:
+
+        # Crear malla con np.mgrid
+        grid_x, grid_y = np.mgrid[bounds[0]:bounds[2]:step*1j, bounds[1]:bounds[3]:step*1j]
+
+        # Crear coordenadas de la malla
+        grid_coords = np.column_stack([grid_x.ravel(), grid_y.ravel()])
+
+        return grid_x, grid_y, grid_coords
+
+    def predict_grid(
+        model: GaussianProcessRegressor, 
+        grid: Tuple[np.ndarray, np.ndarray, np.ndarray]
+    ) -> Tuple[np.ndarray, np.ndarray]:
+
+        # Desempaquetar la tupla grid
+        grid_x, grid_y, _ = grid
+
+        # Preparar puntos para predicción
+        points = np.column_stack((grid_x.ravel(), grid_y.ravel()))
+
+        # Predecir
+        z, ss = model.predict(points, return_std=True)
+
+        return z, ss
+
+    def convert_to_2d_grid(
+        data: Tuple[np.ndarray, np.ndarray], 
+        grid_shape: Tuple[int, int]
+    ) -> Tuple[np.ndarray, np.ndarray]:
+
+        # Desempaquetar la tupla data
+        z, ss = data 
+
+        # Convertir a grillas 2D los valores predichos
+        z_2d = z.reshape(grid_shape)
+
+        # Convertir a grillas 2D las desviacciones estándar(incertidumbre)
+        ss_2d = ss.reshape(grid_shape)
+
+        return z_2d, ss_2d
